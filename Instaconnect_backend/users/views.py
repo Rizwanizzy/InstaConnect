00d4aed9d374b98591a95c40175e48b32bffd01b
Mohammed Rizwan <mohammedrizwanizzy@gmail.com>
2023-09-12 11:40:49 +0530

    backend authentication completed and frontend cleanup finished

---
+++ Instaconnect_backend/users/views.py
@@ -0,0 +1,61 @@
+from rest_framework.response import Response
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import api_view
+
+from rest_framework.views import APIView
+from rest_framework import permissions,status,generics
+
+from .serializer import UserSerializer,UserCreateSerializer
+from .models import UserAccount
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        token['is_superuser'] = user.is_superuser
+        token['email'] = user.email
+        # ...
+        usr = UserSerializer(user)
+        if usr.data['is_active']:
+            return token
+        else:
+            return Response('You are Blocked by Admin',status=status.HTTP_404_NOT_FOUND)
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'api/token/',
+        'api/token/refresh/',
+        'api/token/verify/',
+        'users/me/',
+        'register/'
+    ]
+    
+    return Response(routes)
+
+
+class RegisterUser(APIView):
+    def post(self,request):
+        serializer = UserCreateSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        user = serializer.create(serializer.validated_data)
+        user = UserSerializer(user)
+        return Response(user.data,status=status.HTTP_201_CREATED)
+    
+class RetrieveUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        user = UserSerializer(user)
+        return Response(user.data, status=status.HTTP_200_OK)
